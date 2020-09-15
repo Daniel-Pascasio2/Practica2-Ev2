1a38dec7029200468b524f8a141effde31f5d78e
AndresKenichi <oyakatasama.kai@gmail.com>
2020-09-14 22:04:15 -0600

    Creamos el disenio de la actividad 1 y agregamos la Actividad2

--- app/src/main/java/com/example/paisescentroamerica/MainActivity.java
+++ app/src/main/java/com/example/paisescentroamerica/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.paisescentroamerica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAvanza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAvanzar = (Button) findViewById(R.id.btnAvanza);
+
+        btnAvanzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent unouno = new Intent(MainActivity.this, Actividad2.class);
+                startActivity(unouno);
+            }
+        });
+
+
+
+
     }
 }
